78e098a134b6af11aa9aeaebb0b4e09dc09a9307
Vusal <vusal.novruzov.1@iliauni.edu.ge>
2023-12-26 18:01:58 +0400

    type

--- lib/mongodb/database/index.ts
+++ lib/mongodb/database/index.ts
@@ -1,4 +1,4 @@
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
 
 // const MONGODB_URI = process.env.MONGODB_URI;
 // let cached = (global as any).mongoose || { conn: null, promise: null };
@@ -20,19 +20,17 @@
 //   return cached.conn;
 // };
 
-import mongoose from "mongoose";
-
 let isConnected = false;
 
 export const connectDb = async () => {
   mongoose.set("strictQuery", true);
-  if (!process.env.MONGODB_URL) {
+  if (!process.env.MONGODB_URI) {
     console.log("Mongo db url is not found");
   }
   if (isConnected) console.log("Already connected to mongodb");
 
   try {
-    await mongoose.connect(process.env.MONGODB_URL || "");
+    await mongoose.connect(process.env.MONGODB_URI || "");
     isConnected = true;
   } catch (error) {
     console.log(error);
